df9867d8d975b4b50f9e91edb92089e64d186333
ujjwal <ujjwalmaity98@gmail.com>
2019-10-30 16:42:47 +0530

    Do simple data binding

--- app/src/main/java/dev/ujjwal/databindingdemo1/MainActivity.java
+++ app/src/main/java/dev/ujjwal/databindingdemo1/MainActivity.java
@@ -1,14 +1,23 @@
 package dev.ujjwal.databindingdemo1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import dev.ujjwal.databindingdemo1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Student student = new Student("Ujjwal Maity", "ujjwalmaity@gmail.com");
+        activityMainBinding.setStudentVariable(student);
     }
 }
